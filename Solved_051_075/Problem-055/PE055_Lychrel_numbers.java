372fff69d3fa5fe010dae7b9f0698a6aad338279
aks47u <aks47u@hotmail.co.uk>
2014-05-04 21:10:11 +0100

    Create PE055_Lychrel_numbers.java

---
+++ Solved_051_075/Problem-055/PE055_Lychrel_numbers.java
@@ -0,0 +1,71 @@
+package Solved_051_075;
+
+import java.math.BigInteger;
+
+/**
+ * Lychrel numbers
+ * Problem 55
+ * 
+ * If we take 47, reverse and add, 47 + 74 = 121, which is palindromic.
+ * 
+ * Not all numbers produce palindromes so quickly. For example,
+ * 
+ * 349 + 943 = 1292
+ * 1292 + 2921 = 4213
+ * 4213 + 3124 = 7337
+ * 
+ * That is, 349 took three iterations to arrive at a palindrome.
+ * 
+ * Although no one has proved it yet, it is thought that some numbers, like 196,
+ * never produce a palindrome. A number that never forms a palindrome through
+ * the reverse and add process is called a Lychrel number. Due to the
+ * theoretical nature of these numbers, and for the purpose of this problem, we
+ * shall assume that a number is Lychrel until proven otherwise. In addition you
+ * are given that for every number below ten-thousand, it will either (i) become
+ * a palindrome in less than fifty iterations, or, (ii) no one, with all the
+ * computing power that exists, has managed so far to map it to a palindrome. In
+ * fact, 10677 is the first number to be shown to require over fifty iterations
+ * before producing a palindrome: 4668731596684224866951378664 (53 iterations,
+ * 28-digits).
+ * 
+ * Surprisingly, there are palindromic numbers that are themselves Lychrel
+ * numbers; the first example is 4994.
+ * 
+ * How many Lychrel numbers are there below ten-thousand?
+ * 
+ * NOTE: Wording was modified slightly on 24 April 2007 to emphasise the
+ * theoretical nature of Lychrel numbers.
+ */
+public class PE055_Lychrel_numbers {
+	public static void main(String[] args) {
+		long start = System.nanoTime();
+
+		int result = 0;
+		for (int i = 195; i < 10000; i++) {
+			StringBuffer sb;
+			String s;
+			BigInteger next = new BigInteger(new Integer(i).toString());
+			for (int j = 0; j < 50; j++) {
+				BigInteger rev = new BigInteger(new StringBuffer(
+						next.toString()).reverse().toString());
+				next = next.add(rev);
+				s = next.toString();
+				sb = new StringBuffer(s).reverse();
+
+				if (s.equals(sb.toString())) {
+					break;
+				}
+
+				if (j == 49) {
+					result++;
+				}
+			}
+		}
+
+		long end = System.nanoTime();
+		long runtime = end - start;
+		System.out.println(result);
+		System.out.println("Runtime: " + runtime / 1000000 + "ms (" + runtime
+				+ "ns)");
+	}
+}
