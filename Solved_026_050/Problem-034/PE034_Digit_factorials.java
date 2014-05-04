ff1118e6d749a164229fa2acbffa6937eefab4f8
aks47u <aks47u@hotmail.co.uk>
2014-05-04 20:54:09 +0100

    Create PE034_Digit_factorials.java

---
+++ Solved_026_050/Problem-034/PE034_Digit_factorials.java
@@ -0,0 +1,50 @@
+package Solved_026_050;
+
+/**
+ * Digit factorials
+ * Problem 34
+ * 
+ * 145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+ * 
+ * Find the sum of all numbers which are equal to the sum of the factorial of
+ * their digits.
+ * 
+ * Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+ */
+public class PE034_Digit_factorials {
+	public static void main(String[] args) {
+		long start = System.nanoTime();
+
+		long result = 0;
+		int[] fact = new int[10];
+
+		for (int i = 0; i < 10; ++i) {
+			fact[i] = 1;
+
+			for (int j = 2; j <= i; ++j) {
+				fact[i] = fact[i] * j;
+			}
+		}
+
+		for (int i = 3; i < 10000000; ++i) {
+			String digits = Integer.toString(i);
+			long num = 0;
+
+			for (int j = 0; j < digits.length(); ++j) {
+				char c = digits.charAt(j);
+				int digit = (int) (c - '0');
+				num += fact[digit];
+			}
+
+			if (num == i) {
+				result += i;
+			}
+		}
+
+		long end = System.nanoTime();
+		long runtime = end - start;
+		System.out.println(result);
+		System.out.println("Runtime: " + runtime / 1000000 + "ms (" + runtime
+				+ "ns)");
+	}
+}
