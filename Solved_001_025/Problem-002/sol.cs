2790e38093fd37246d3e4401db7e71b32a9283dd
Conor Fitzpatrick <aks47u@hotmail.co.uk>
2014-06-11 08:42:23 +0100

    Create sol.cs

---
+++ Solved_001_025/Problem-002/sol.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Test {
+	public static void Main() {
+		int limit = 4000000;
+        int previous = 1;
+        int result = 0;
+        int x = 0;
+
+        for (int i = 1; i <= limit; i += previous) {
+            if(x > 0) {
+				previous = i - previous;
+			}
+			
+            x++;
+			
+            if (i % 2 == 0) {
+				result += i;
+			}
+        }
+		
+        Console.WriteLine(result);
+	}
+}
