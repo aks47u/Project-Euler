49f97386b05f7347d25e9d5aa9689ad41fa1c60e
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-07-28 10:55:57 +0100

    formatting

--- Solved_001_025/Problem-001/sol.c
+++ Solved_001_025/Problem-001/sol.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
-int main (void) {
-    int result = 0;
-    int i;
-    
-    for(i = 0; i < 1000; i++) {
-        if (i % 3 == 0 || i % 5 == 0) {
-            result += i;
-        }
-    }
-    
-    printf("%d\n", result);
-    
-    return 0;
+int main(void) {
+	int result = 0;
+
+	for (int i = 1; i < 1000; i++) {
+		if (i % 3 == 0 || i % 5 == 0) {
+			result += i;
+		}
+	}
+
+	printf("%d\n", result);
+
+	return 0;
 }
