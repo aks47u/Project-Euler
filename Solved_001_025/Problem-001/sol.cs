fc3e6032801071ebc0ed9bef67a82a9705e8dfb3
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-07-28 10:57:27 +0100

    formatting

--- Solved_001_025/Problem-001/sol.cs
+++ Solved_001_025/Problem-001/sol.cs
@@ -1,15 +1,15 @@
 using System;
 
 class Program {
-    static void Main() {
-        int result = 0;
-    	
-        for (int num = 0; num < 1000; num++) {
-            if (num % 3 == 0 || num % 5 == 0) {
-                    result += num;
-            }
-        }
-		
-        Console.WriteLine(result);
-    }
+	static void Main() {
+		int result = 0;
+
+		for (int i = 1; i < 1000; i++) {
+			if (i % 3 == 0 || i % 5 == 0) {
+				result += i;
+			}
+		}
+
+		Console.WriteLine(result);
+	}
 }
