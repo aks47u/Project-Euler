8d87cc10792afc0ea1e44d59b73568b82105681c
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-07-29 09:31:03 +0100

    formatting

--- Solved_001_025/Problem-002/sol.cs
+++ Solved_001_025/Problem-002/sol.cs
@@ -6,19 +6,19 @@ public class Test {
 		int previous = 1;
 		int result = 0;
 		int x = 0;
-		
+
 		for (int i = 1; i <= limit; i += previous) {
 			if(x > 0) {
 				previous = i - previous;
 			}
-			
+
 			x++;
-			
+
 			if (i % 2 == 0) {
 				result += i;
 			}
 		}
-		
+
 		Console.WriteLine(result);
 	}
 }
