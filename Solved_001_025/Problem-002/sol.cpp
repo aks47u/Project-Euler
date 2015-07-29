4f992e45db31de100d190c112bd1a9330dc14e15
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-07-29 09:30:24 +0100

    formatting

--- Solved_001_025/Problem-002/sol.cpp
+++ Solved_001_025/Problem-002/sol.cpp
@@ -6,19 +6,19 @@ int main() {
 	int i = 0, j = 1;
 	int current = 0;
 	int result = 0;
-	
-	while(current < 4000000) {
+
+	while (current < 4000000) {
 		current = i + j;
-		
+
 		if (current % 2 == 0) {
 			result += current;
 		}
-		
+
 		i = j;
 		j = current;
 	}
-	
+
 	cout << result << endl;
-	
+
 	return 0;
 }
