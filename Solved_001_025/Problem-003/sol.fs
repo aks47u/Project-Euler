5a96b28a22b39a9523dbcdea77a374617fc88dbd
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-07-30 09:44:47 +0100

    formatting

--- Solved_001_025/Problem-003/sol.fs
+++ Solved_001_025/Problem-003/sol.fs
@@ -4,10 +4,9 @@ let rec isprime (x:int64) i =
             false
         else
             isprime x (i + 1L)
-            
     else
         true
-        
+
 let primefactor (x:int64) = 
     let mutable i:int64 = 2L
     let mutable n:int64 = x
@@ -16,19 +15,17 @@ let primefactor (x:int64) =
     while (i < x) do
         if (x % i = 0L) then
             let testprime:int64 = x / i
-            
+
             if (isprime testprime 2L) then
                 highest <- testprime
-                
                 n <- n / i
-                
                 i <- 2L
-                
+
         if (highest = 1L) then
             i <- i + 1L
         else
             i <- x
-            
+
     highest
-    
+
 printfn "%d" (primefactor 600851475143L)
