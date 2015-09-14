323d8dcb49a664aec493b31dec339bc18b065cf1
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-09-14 09:59:19 +0100

    formatting

--- Solved_101_125/Problem-118/PE118_Pandigital_prime_sets.java
+++ Solved_101_125/Problem-118/PE118_Pandigital_prime_sets.java
@@ -41,12 +41,10 @@ public class PE118_Pandigital_prime_sets {
 	private static boolean isPrime(int num) {
 		if (num == 2) {
 			return true;
-
 		}
 
 		if (((num % 2) == 0) || num < 2) {
 			return false;
-
 		}
 
 		for (int t = 3; t * t <= num; t += 2) {
