43333a6d5fb0a6592fe0a09542dcae238434e32c
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-10-08 08:56:25 +0100

    formatting

--- Solved_151_175/Problem-162/PE162_Hexadecimal_numbers.java
+++ Solved_151_175/Problem-162/PE162_Hexadecimal_numbers.java
@@ -23,11 +23,8 @@ package Solved_151_175;
  * not: 1a3f and not 0x1a3f and not $1A3F and not #1A3F and not 0000001A3F)
  */
 public class PE162_Hexadecimal_numbers {
-	private static final int _NIL = 0;
-	private static final int _0 = 1;
-	private static final int _1 = 2;
-	private static final int _A = 4;
-	private static final long[] _powers = new long[15];
+	private static final int _NIL = 0, _0 = 1, _1 = 2, _A = 4;
+	private static final long[] _POWERS = new long[15];
 
 	public static void main(String[] args) {
 		long start = System.nanoTime();
@@ -37,15 +34,15 @@ public class PE162_Hexadecimal_numbers {
 		long end = System.nanoTime();
 		long runtime = end - start;
 		System.out.println(result);
-		System.out.println("Runtime: " + runtime / 1000000 + "ms (" + runtime
-				+ "ns)");
+		System.out.println(
+				"Runtime: " + runtime / 1000000 + "ms (" + runtime + "ns)");
 	}
 
-	static{
-		_powers[0] = 16L;
+	static {
+		_POWERS[0] = 16L;
 
-		for (int k = 1; k < _powers.length; k++) {
-			_powers[k] = 16 * _powers[k - 1];
+		for (int k = 1; k < _POWERS.length; k++) {
+			_POWERS[k] = 16 * _POWERS[k - 1];
 		}
 	}
 
@@ -61,30 +58,31 @@ public class PE162_Hexadecimal_numbers {
 		int k = digits - 1;
 
 		switch (flags) {
-		case _NIL:
-			return (13 * count(_NIL, true, k))
-					+ count(allowLeadingZero ? _0 : _NIL, allowLeadingZero, k)
-					+ count(_1, true, k) + count(_A, true, k);
-		case _0:
-			return (14 * count(_0, true, k)) + count(_0 + _1, true, k)
-					+ count(_0 + _A, true, k);
-		case _1:
-			return (14 * count(_1, true, k)) + count(_1 + _0, true, k)
-					+ count(_1 + _A, true, k);
-		case _0 + _1:
-			return (15 * count(_0 + _1, true, k))
-					+ count(_0 + _1 + _A, true, k);
-		case _A:
-			return (14 * count(_A, true, k)) + count(_A + _0, true, k)
-					+ count(_A + _1, true, k);
-		case _0 + _A:
-			return (15 * count(_0 + _A, true, k))
-					+ count(_0 + _1 + _A, true, k);
-		case _1 + _A:
-			return (15 * count(_1 + _A, true, k))
-					+ count(_0 + _1 + _A, true, k);
-		case _0 + _1 + _A:
-			return _powers[k];
+			case _NIL:
+				return (13 * count(_NIL, true, k))
+						+ count(allowLeadingZero ? _0 : _NIL, allowLeadingZero,
+								k)
+						+ count(_1, true, k) + count(_A, true, k);
+			case _0:
+				return (14 * count(_0, true, k)) + count(_0 + _1, true, k)
+				+ count(_0 + _A, true, k);
+			case _1:
+				return (14 * count(_1, true, k)) + count(_1 + _0, true, k)
+				+ count(_1 + _A, true, k);
+			case _0 + _1:
+				return (15 * count(_0 + _1, true, k))
+						+ count(_0 + _1 + _A, true, k);
+			case _A:
+				return (14 * count(_A, true, k)) + count(_A + _0, true, k)
+				+ count(_A + _1, true, k);
+			case _0 + _A:
+				return (15 * count(_0 + _A, true, k))
+						+ count(_0 + _1 + _A, true, k);
+			case _1 + _A:
+				return (15 * count(_1 + _A, true, k))
+						+ count(_0 + _1 + _A, true, k);
+			case _0 + _1 + _A:
+				return _POWERS[k];
 		}
 
 		return 0L;
